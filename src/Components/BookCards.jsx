9ce38fd1e0051cdf7b4f7370dbfdf2ffa3798042
sourabh1428 <sppathak1428@gmail.com>
2023-11-26 14:32:25 +0530

    dsa

--- src/Components/BookCards.jsx
+++ src/Components/BookCards.jsx
@@ -19,7 +19,7 @@ const BookCards = ({details,setData,setFlag,moredetails,key}) => {
 
 
     return (
-        <div  className= {`card1 ${key % 2 === 0 ? 'even' : 'odd'}`}>
+        <div  className= {`card1 ${key % 2 === 0 ? 'even' : 'odd'}`} >
             <div className='card1l'><img src={details.imageLinks.thumbnail} alt="" /></div>
             <div className="card1r">
                 <h1>{details.title && truncateDescription(details.title, 3)}</h1>
